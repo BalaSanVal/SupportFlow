184f1350ace3419cebc61e7d1f7974dc32a2a830
BalaSanVal <balasanval_24@hotmail.com>
2026-08-03 20:21:09 -0600

    chore: configure local development environment

---
+++ backend/src/test/java/com/supportflow/SupportflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supportflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
